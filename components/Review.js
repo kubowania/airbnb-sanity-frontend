0a41bc964135a6ef7d352016423a2c87e0188040
diipanshuu <diipanshuu@gmail.com>
2022-07-05 21:04:20 +0530

    couple of changes in for review!

--- components/Review.js
+++ components/Review.js
@@ -2,17 +2,31 @@ import { urlFor } from "../sanity"
 
 const Review = ({ review }) => {
 
-  return (
-    <div className="review-box">
-      <h1>{review.rating}</h1>
-      <h2>{review.traveller.name}</h2>
-      <img
-        src={urlFor(review.traveller.image)
-          .width(50)
-          .height(50)
-          .crop("focalpoint")
-          .auto("format")}
-      />
+//   return (
+//     <div className="review-box">
+//       <h1>{review.rating}</h1>
+//       <h2>{review.traveller.name}</h2>
+//       <img
+//         src={urlFor(review.traveller.image)
+//           .width(50)
+//           .height(50)
+//           .crop("focalpoint")
+//           .auto("format")}
+//       />
+//     </div>
+//   )
+// }
+
+    return (
+        <div className="review-box">
+        <h2>{review.rating}</h2>
+        <img
+             src={urlFor(review.traveller.name)
+                 .width(50)
+                 .height(50)
+                 .crop("focalpoint")
+                 .auto("format")}
+         />
     </div>
   )
 }
